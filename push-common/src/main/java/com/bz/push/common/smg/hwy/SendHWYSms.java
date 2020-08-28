cea7dfe740c9852634f027125069a2e5b86ba0a4
songyixian <kangxizhizi@163.com>
2020-08-28 15:42:30 +0800

    catch HWY message log

--- push-common/src/main/java/com/bz/push/common/smg/hwy/SendHWYSms.java
+++ push-common/src/main/java/com/bz/push/common/smg/hwy/SendHWYSms.java
@@ -1,9 +1,13 @@
 package com.bz.push.common.smg.hwy;
 
 import com.bz.push.common.utils.HttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SendHWYSms {
-	
+
+    private static final Logger logger = LoggerFactory.getLogger(SendHWYSms.class);
+
 	public static String sendSms(String mobile,String yzm,String statusCallBack) throws Exception {
 		String url = "https://rtcsms.cn-north-1.myhuaweicloud.com:10743/sms/batchSendSms/v1"; //APP接入地址+接口访问URI
         String appKey = "lHt2ECZ9sD4cTXAy521aBAful60c"; //APP_Key
@@ -15,8 +19,9 @@ public class SendHWYSms {
         String receiver = "+86"+mobile; //短信接收人号码
         String templateParas = "["+yzm+"]"; //模板变量
         String body = HWYRequestBody.buildRequestBody(sender, receiver, templateId, templateParas, statusCallBack, signature);
+        logger.info(body);
         if (null == body || body.isEmpty()) {
-            System.out.println("body is null.");
+            logger.info("body is null.");
             return null;
         }
         
