15f60eb14520fc0894e581327070cdf3600bc050
songyixian <kangxizhizi@163.com>
2020-08-28 14:08:45 +0800

    change the message template

--- push-common/src/main/java/com/bz/push/common/smg/hwy/SendHWYSms.java
+++ push-common/src/main/java/com/bz/push/common/smg/hwy/SendHWYSms.java
@@ -8,9 +8,9 @@ public class SendHWYSms {
 		String url = "https://rtcsms.cn-north-1.myhuaweicloud.com:10743/sms/batchSendSms/v1"; //APP接入地址+接口访问URI
         String appKey = "lHt2ECZ9sD4cTXAy521aBAful60c"; //APP_Key
         String appSecret = "ScZV6UgVS26Y1U12VrV7fiIN6C3a"; //APP_Secret
-        String sender = "99200620888880000482"; //国内短信签名通道号或国际/港澳台短信通道号
-        String templateId = " dedc5370c30a4451a09443ef684c2756"; //模板ID
-        String signature = "TEST_TEMPLATE_QiLin"; //签名名称
+        String sender = "8820082718614"; //国内短信签名通道号或国际/港澳台短信通道号
+        String templateId = " d52c03023ba141af84b7e514318be3e1"; //模板ID
+        String signature = "川维科技"; //签名名称
         
         String receiver = "+86"+mobile; //短信接收人号码
         String templateParas = "["+yzm+"]"; //模板变量
